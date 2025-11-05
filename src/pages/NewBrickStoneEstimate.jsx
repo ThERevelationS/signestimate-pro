d3a6f3908c52c987e4b380b1c3c38025fc0f88d6
Brett Heinlein <brettheinlein1@gmail.com>
2025-11-05 14:13:48 +0000

    File changes

--- src/pages/NewBrickStoneEstimate.jsx
+++ src/pages/NewBrickStoneEstimate.jsx
@@ -4,7 +4,7 @@ import { BrickStoneProject, BrickStoneInventory, Settings } from "@/entities/all
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { createPageUrl } from "@/utils";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } => {
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
