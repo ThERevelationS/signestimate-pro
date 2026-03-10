ea15e51216278f1caa2868b62b46880a5256af6e
Brett Heinlein <brettheinlein1@gmail.com>
2026-03-10 16:50:57 +0000

    File changes

--- src/components/ui/sidebar.jsx
+++ src/components/ui/sidebar.jsx
@@ -390,7 +390,7 @@ const sidebarMenuButtonVariants = cva(
 )
 
 const SidebarMenuButton = React.forwardRef(function SidebarMenuButton(
-  { asChild = false, isActive = false, variant = "default", size = "default", tooltip, className, ...props },
+  { asChild = false, isActive = false, variant = "default", size = "default", tooltip, className, id, ...props },
   ref
 ) {
   const Comp = asChild ? Slot : "button"
