531dd1227e401bf911ff37880daa07fb0ca74ade
Brett Heinlein <brettheinlein1@gmail.com>
2026-03-12 17:09:41 +0000

    File changes

--- src/pages/NewFoundationEstimate.jsx
+++ src/pages/NewFoundationEstimate.jsx
@@ -15,6 +15,7 @@ import { createPageUrl } from '@/utils';
 import WallSection from '@/components/WallSection';
 import FoundationWalls3DViewer from '@/components/FoundationWalls3DViewer';
 import { UnsavedChangesContext } from '@/components/UnsavedChangesContext';
+import SummaryTab from '@/components/foundation/SummaryTab';
 
 const FoundationProjectEntity = base44.entities.FoundationProject;
 const FoundationInventoryEntity = base44.entities.FoundationInventory;
